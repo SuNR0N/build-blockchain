cadd46f34317906c1028f93b8e73d940a3e69805
Norbert Annus <Norbert.Annus@wipro.com>
2018-06-25 16:06:58 +0100

    Add test for calculateBalance

--- src/models/Wallet.test.ts
+++ src/models/Wallet.test.ts
@@ -1,9 +1,9 @@
 import { INITIAL_BALANCE } from '../config';
 import {
+  Blockchain,
   TransactionPool,
   Wallet,
 } from './';
-import { Blockchain } from './Blockchain';
 
 describe('Wallet', () => {
   describe('constructor', () => {
@@ -74,6 +74,14 @@ describe('Wallet', () => {
       wallet = new Wallet();
     });
 
+    it('should call the calculateBalance function with the blockchain', () => {
+      const calculateBalanceSpy = jest.spyOn(wallet, 'calculateBalance');
+
+      wallet.createTransaction(recipient, 1000, blockchain, transactionPool);
+
+      expect(calculateBalanceSpy).toHaveBeenCalledWith(blockchain);
+    });
+
     it('should log a message if the provided amount exceeds the current balance', () => {
       const logSpy = jest.spyOn(console, 'log');
 
@@ -127,6 +135,64 @@ describe('Wallet', () => {
       const wallet = Wallet.blockchainWallet();
 
       expect(wallet.publicKey).toBe('blockchain-wallet');
+    });
+  });
+
+  describe('calculateBalance', () => {
+    const amountX = 100;
+    const repeat = 3;
+    let blockchain: Blockchain;
+    let senderWallet: Wallet;
+    let recipientWallet: Wallet;
+    let transactionPool: TransactionPool;
+
+    beforeEach(() => {
+      transactionPool = new TransactionPool();
+      blockchain = new Blockchain();
+      senderWallet = new Wallet();
+      recipientWallet = new Wallet();
+      for (let i = 0; i < repeat; i++) {
+        senderWallet.createTransaction(recipientWallet.publicKey, amountX, blockchain, transactionPool);
+      }
+      blockchain.addBlock(transactionPool.transactions);
+    });
+
+    it('should calculate the balance for blockchain transactions matching the recipient', () => {
+      const recipientBalance = recipientWallet.calculateBalance(blockchain);
+
+      expect(recipientBalance).toBe(INITIAL_BALANCE + repeat * amountX);
+    });
+
+    it('should calculate the balance for blockchain transactions matching the sender', () => {
+      const senderBalance = senderWallet.calculateBalance(blockchain);
+
+      expect(senderBalance).toBe(INITIAL_BALANCE - repeat * amountX);
+    });
+
+    describe('and the recipient makes a transaction', () => {
+      const amountY = 75;
+      let recipientBalance: number;
+
+      beforeEach(() => {
+        transactionPool.clear();
+        recipientBalance = recipientWallet.calculateBalance(blockchain);
+        recipientWallet.createTransaction(senderWallet.publicKey, amountY, blockchain, transactionPool);
+        blockchain.addBlock(transactionPool.transactions);
+      });
+
+      describe('and the sender sends an other transaction to the recipient', () => {
+        beforeEach(() => {
+          transactionPool.clear();
+          senderWallet.createTransaction(recipientWallet.publicKey, amountX, blockchain, transactionPool);
+          blockchain.addBlock(transactionPool.transactions);
+        });
+
+        it('should calculate the recipient balance only using transactions since its most recent one', () => {
+          const updatedRecipientBalance = recipientWallet.calculateBalance(blockchain);
+
+          expect(updatedRecipientBalance).toBe(recipientBalance - amountY + amountX);
+        });
+      });
     });
   });
 });
